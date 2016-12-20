f08e7f81f75f64c9e0f1374df6923aa0f30a5572
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-12-20 13:52:56 +0100

    Add color blending functions from StackOverflow

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { assert as t } from "chai";
-import { hexToRgb, rgbToHex } from "../index";
+import { hexToRgb, rgbToHex, shade, blend } from "../index";
 
 describe("rgbToHex", () => {
   it("should convert rgb to hex", () => {
@@ -24,5 +24,59 @@ describe("hexToRgb", () => {
     t.deepEqual(hexToRgb("#000000"), [0, 0, 0]);
 
     t.deepEqual(hexToRgb("#f00"), [255, 0, 0]);
+  });
+});
+
+describe("shade", () => {
+  it("should lighten a hex color", () => {
+    t.equal(shade("#f48000", 0), "f48000");
+    t.equal(shade("f48000", 0), "f48000");
+    t.equal(shade("f48000", .3), "f7a64d");
+    t.equal(shade("f48000", .5), "fac080");
+    t.equal(shade("f48000", .7), "fcd9b3");
+    t.equal(shade("f48000", 1), "ffffff");
+  });
+
+  it("should darken a hex color", () => {
+    t.equal(shade("#f48000", 0), "f48000");
+    t.equal(shade("f48000", 0), "f48000");
+    t.equal(shade("f48000", -.3), "ab5a00");
+    t.equal(shade("f48000", -.5), "7a4000");
+    t.equal(shade("f48000", -.7), "492600");
+    t.equal(shade("f48000", -1), "000000");
+  });
+
+  it("should lighten a rgb color", () => {
+    t.deepEqual(shade([244, 128, 0], 0), [244, 128, 0]);
+    t.deepEqual(shade([244, 128, 0], .3), [247, 166, 77]);
+    t.deepEqual(shade([244, 128, 0], .5), [250, 192, 128]);
+    t.deepEqual(shade([244, 128, 0], .7), [252, 217, 179]);
+    t.deepEqual(shade([244, 128, 0], 1), [255, 255, 255]);
+  });
+
+  it("should darken a rgb color", () => {
+    t.deepEqual(shade([244, 128, 0], 0), [244, 128, 0]);
+    t.deepEqual(shade([244, 128, 0], -.3), [171, 90, 0]);
+    t.deepEqual(shade([244, 128, 0], -.5), [122, 64, 0]);
+    t.deepEqual(shade([244, 128, 0], -.7), [73, 38, 0]);
+    t.deepEqual(shade([244, 128, 0], -1), [0, 0, 0]);
+  });
+});
+
+describe("blend", () => {
+  it("should blend two hex colors together", () => {
+    t.equal(blend("#f48000", "3f83a3", 0), "f48000");
+    t.equal(blend("#f48000", "3f83a3", .2), "d08121");
+    t.equal(blend("#f48000", "3f83a3", .5), "9a8252");
+    t.equal(blend("#f48000", "3f83a3", .7), "758272");
+    t.equal(blend("#f48000", "3f83a3", 1), "3f83a3");
+  });
+
+  it("should blend two rgb colors together", () => {
+    t.deepEqual(blend([244, 128, 0], [63, 131, 163], 0), [244, 128, 0]);
+    t.deepEqual(blend([244, 128, 0], [63, 131, 163], .2), [208, 129, 33]);
+    t.deepEqual(blend([244, 128, 0], [63, 131, 163], .5), [154, 130, 82]);
+    t.deepEqual(blend([244, 128, 0], [63, 131, 163], .7), [117, 130, 114]);
+    t.deepEqual(blend([244, 128, 0], [63, 131, 163], 1), [63, 131, 163]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Hex & RGB conversion taken from:
+// http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+
 function componentToHex(c: number) {
   const hex = c.toString(16);
   return hex.length === 1 ? "0" + hex : hex;
@@ -22,4 +25,95 @@ export function hexToRgb(hex: string) {
   const b = bigint & 255;
 
   return [r, g, b];
+}
+
+// Shade functions taken from:
+// http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+
+function shadeHexColor(color: string, percent: number) {
+  if (color[0] === "#") {
+    color = color.slice(1);
+  }
+
+  const f = parseInt(color, 16);
+  const t = percent < 0 ? 0 : 255;
+  const p = percent < 0 ? percent * -1 : percent;
+
+  const R = f >> 16;
+  const G = f >> 8 & 0x00FF;
+  const B = f & 0x0000FF;
+
+  return "" + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
+}
+
+function shadeRGBColor(R: number, G: number, B: number, percent: number) {
+  const t = percent < 0 ? 0 : 255;
+  const p = percent < 0 ? percent * -1 : percent;
+
+  R = Math.round((t - R) * p) + R;
+  G = Math.round((t - G) * p) + G;
+  B = Math.round((t - B) * p) + B;
+
+  return [R, G, B];
+}
+
+/** Darken or lighten a hex or rgb color. Percentage range is `-1.0 - 1.0` */
+export function shade(color: string, percent: number): string;
+export function shade(color: number[], percent: number): number[];
+export function shade(color: string | number[], percent: number) {
+  if (typeof color === "string") {
+    return shadeHexColor(color, percent);
+  }
+
+  return shadeRGBColor(color[0], color[1], color[2], percent);
+}
+
+function blendHexColors(color1: string, color2: string, percent: number) {
+  if (color1[0] === "#") {
+    color1 = color1.slice(1);
+  }
+
+  if (color2[0] === "#") {
+    color2 = color2.slice(1);
+  }
+
+  const f = parseInt(color1, 16);
+  const t = parseInt(color2, 16);
+
+  const R1 = f >> 16;
+  const G1 = f >> 8 & 0x00FF;
+  const B1 = f & 0x0000FF;
+
+  const R2 = t >> 16;
+  const G2 = t >> 8 & 0x00FF;
+  const B2 = t & 0x0000FF;
+
+  return (0x1000000 + (Math.round((R2 - R1) * percent) + R1) * 0x10000 + (Math.round((G2 - G1) * percent) + G1) * 0x100 + (Math.round((B2 - B1) * percent) + B1)).toString(16).slice(1);
+}
+
+function blendRGBColors(color1: number[], color2: number[], percent: number) {
+  const R1 = color1[0];
+  const G1 = color1[1];
+  const B1 = color1[2];
+
+  const R2 = color2[0];
+  const G2 = color2[1];
+  const B2 = color2[2];
+
+  const R = (Math.round((R2 - R1) * percent) + R1);
+  const G = (Math.round((G2 - G1) * percent) + G1);
+  const B = (Math.round((B2 - B1) * percent) + B1);
+
+  return [R, G, B];
+}
+
+/** Blend two colors together. Percentage range is from `0 - 1` */
+export function blend(color1: string, color2: string, percent: number): string;
+export function blend(color1: number[], color2: number[], percent: number): number[];
+export function blend(color1: string | number[], color2: string | number[], percent: number) {
+  if (typeof color1 === "string" && typeof color2 === "string") {
+    return blendHexColors(color1, color2, percent);
+  }
+
+  return blendRGBColors(color1 as number[], color2 as number[], percent);
 }
