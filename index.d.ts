44527a6bc34a99022517c3121463c0d9109e37ff
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-12-25 13:26:36 +0100

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -9,11 +9,23 @@ declare module "color-helpers" {
   export function hslToHsv(h: number, s: number, l: number): number[];
   /** Convert hsv color to rgb. */
   export function hsvToRgb(h: number, s: number, v: number): number[];
-  /** Convert rgb to hsv. */
+  /**
+   * Convert color from rgb to hsv.
+   *
+   * *red*: 0 - 255, *green*: 0 - 255, *blue*: 0 - 255
+   */
   export function rgbToHsv(r: number, g: number, b: number): number[];
-  /** Convert rgb to hsl. */
+  /**
+   * Convert color from rgb to hsl.
+   *
+   * *red*: 0 - 255, *green*: 0 - 255, *blue*: 0 - 255
+   */
   export function rgbToHsl(r: number, g: number, b: number): number[];
-  /** Convert hsl to rgb. */
+  /**
+   * Convert color from hsl to rgb.
+   *
+   * *Hue*: 0 - 360, *Saturation*: 0 - 1, *Lightness*: 0 - 1
+   */
   export function hslToRgb(h: number, s: number, l: number): number[];
 
   /** Darken or lighten a hex or rgb color. Percentage range is `-1.0 - 1.0` */
